b479b0f8ca119d3370ce9fc51c5d0bef5fae854f
oriel <orielsilvajr@gmail.com>
2023-12-28 14:32:44 -0500

    Setup complete

--- main.js
+++ main.js
@@ -5,3 +5,33 @@ const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const path = require('path');
 const url = require('url');
+
+let win;
+
+function createWindow() {
+    win = new BrowserWindow();
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+    win.on('closed', () => {
+        win = null;
+    });
+};
+
+app.on('ready', createWindow);
+
+//This is for mac users
+//If the platform is not equal to mac OS 'darwin' then close app (When clicking x to close)
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (win === null) {
+        createWindow;
+    }
+});
