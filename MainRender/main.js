11d12c6cb4cacddec07dbb0b00db2875c74ba662
oriel <orielsilvajr@gmail.com>
2023-12-28 17:19:06 -0500

    main render folder created

---
+++ MainRender/main.js
@@ -0,0 +1,41 @@
+console.log('This bitch is running oh o ok')
+
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require('path');
+const url = require('url');
+
+let win;
+
+function createWindow() {
+    win = new BrowserWindow();
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+    win.on('closed', () => {
+        win = null;
+    });
+};
+
+//Opens Dev tools in browser window when app is open
+win.webContents.openDevTools();
+
+
+app.on('ready', createWindow);
+
+//This is for mac users
+//If the platform is not equal to mac OS 'darwin' then close app (When clicking x to close)
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (win === null) {
+        createWindow;
+    }
+});
