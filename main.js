ef5d92a24536c259ede1b204d06711e942813fc3
oriel <orielsilvajr@gmail.com>
2023-12-28 13:12:38 -0500

    first

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log('This bitch is running oh o ok')
+
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require('path');
+const url = require('url');
